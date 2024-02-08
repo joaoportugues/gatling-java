6ae8bd8690c7c2467604aa960d58336672f60ba9
Joe <joaomatiasportugues@gmail.com>
2024-02-08 20:38:58 +0100

    init

---
+++ target/gatling/testing-20240208193758415/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1707421080000,1],[1707421081000,1],[1707421082000,1],[1707421083000,1],[1707421084000,1],[1707421085000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
